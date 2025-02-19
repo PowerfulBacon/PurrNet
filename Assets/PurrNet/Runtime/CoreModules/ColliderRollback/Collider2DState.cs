50b7245c923341d23b1e1225f80a7c3532708949
BlenMiner <blenminer123@gmail.com>
2025-02-19 17:21:25 +0100

    separate collider module to multiple files

--- Assets/PurrNet/Runtime/CoreModules/ColliderRollback/Collider2DState.cs
+++ Assets/PurrNet/Runtime/CoreModules/ColliderRollback/Collider2DState.cs
@@ -17,5 +17,16 @@ namespace PurrNet.Modules
             scale = trs.localScale;
             enabled = collider.enabled;
         }
+
+        public Collider2DState Interpolate(Collider2DState stateB, float tickFraction)
+        {
+            return new Collider2DState
+            {
+                position = Vector2.Lerp(position, stateB.position, tickFraction),
+                rotation = Mathf.Lerp(rotation, stateB.rotation, tickFraction),
+                scale = Vector2.Lerp(scale, stateB.scale, tickFraction),
+                enabled = tickFraction > 0.5f ? stateB.enabled : enabled
+            };
+        }
     }
 }
--- Assets/PurrNet/Runtime/CoreModules/ColliderRollback/RollbackModule.cs
+++ Assets/PurrNet/Runtime/CoreModules/ColliderRollback/RollbackModule.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 namespace PurrNet.Modules
 {
-    public class RollbackModule : INetworkModule
+    public partial class RollbackModule : INetworkModule
     {
         PhysicsScene _physicsScene;
 
@@ -24,41 +24,44 @@ namespace PurrNet.Modules
             _physicsScene = scene.GetPhysicsScene();
         }
         
-        public void Enable(bool asServer)
-        {
-        }
+        public void Enable(bool asServer) { }
 
-        public void Disable(bool asServer)
-        {
-        }
+        public void Disable(bool asServer) { }
         
-        static readonly RaycastHit[] _raycastHits = new RaycastHit[1024];
-        
-        /// <summary>
-        /// Casts a ray, from point origin, in direction direction, of length maxDistance, against all colliders in the scene.
-        /// </summary>
-        public int Raycast(double preciseTick, Ray ray, RaycastHit[] raycastHits, 
-            float maxDistance = float.PositiveInfinity, 
-            int layerMask = Physics.AllLayers,
-            QueryTriggerInteraction queryTriggers = QueryTriggerInteraction.UseGlobal)
+        public bool TryGetColliderState(double preciseTick, Collider2D collider, out Collider2DState state)
         {
-            if (!_physicsScene.IsValid())
-                return 0;
+            if (_collider2DStates.TryGetValue(collider, out var history))
+            {
+                uint tick = (uint)preciseTick;
+                uint tickNext = tick + 1;
+                float tickFraction = (float)(preciseTick - tick);
+
+                bool hasStateA = history.TryGet(tick, out var stateA);
+                bool hasStateB = history.TryGet(tickNext, out var stateB);
+                
+                switch (hasStateA)
+                {
+                    case true when hasStateB:
+                        stateA = stateA.Interpolate(stateB, tickFraction);
+                        break;
+                    case false when hasStateB:
+                        stateA = stateB;
+                        break;
+                    case false:
+                    {
+                        state = default;
+                        return false;
+                    }
+                    case true:
+                        break;
+                }
+                
+                state = stateA;
+                return true;
+            }
             
-            int hitCount = _physicsScene.Raycast(ray.origin, ray.direction, raycastHits, maxDistance, layerMask, queryTriggers);
-            
-            uint tick = (uint)preciseTick;
-            float tickFraction = (float)(preciseTick - tick);
-            
-            int colliderCount = _colliders3D.Count;
-            
-            // remove any colliders that we are handling manually
-            hitCount = FilterColliders(hitCount, raycastHits);
-            
-            // handle raycast hits manually
-            hitCount = DoManualRaycasts(ray, raycastHits, maxDistance, layerMask, colliderCount, hitCount, tick, tickFraction, queryTriggers);
-            
-            return hitCount;
+            state = default;
+            return false;
         }
         
         public bool TryGetColliderState(double preciseTick, Collider collider, out Collider3DState state)
@@ -95,133 +98,6 @@ namespace PurrNet.Modules
             
             state = default;
             return false;
-        }
-
-        /// <summary>
-        /// Casts a ray, from point origin, in direction direction, of length maxDistance, against all colliders in the scene.
-        /// </summary>
-        public bool Raycast(double preciseTick, Ray ray, out RaycastHit hit, 
-            float maxDistance = float.PositiveInfinity, 
-            int layerMask = Physics.AllLayers,
-            QueryTriggerInteraction queryTriggers = QueryTriggerInteraction.UseGlobal)
-        {
-            if (!_physicsScene.IsValid())
-            {
-                hit = default;
-                return false;
-            }
-            
-            int hitCount = Raycast(preciseTick, ray, _raycastHits, maxDistance, layerMask, queryTriggers);
-            
-            // return the closest hit
-            if (hitCount > 0)
-            {
-                hit = _raycastHits[0];
-                for (var i = 1; i < hitCount; i++)
-                {
-                    if (_raycastHits[i].distance < hit.distance)
-                        hit = _raycastHits[i];
-                }
-                return true;
-            }
-            
-            hit = default;
-            return false;
-        }
-
-        private int DoManualRaycasts(Ray ray, RaycastHit[] hits, float maxDistance, int layerMask, int colliderCount,
-            int hitCount, uint tick, float tickFraction, QueryTriggerInteraction queryTriggers)
-        {
-            if (queryTriggers == QueryTriggerInteraction.UseGlobal)
-                queryTriggers = Physics.queriesHitTriggers ? QueryTriggerInteraction.Collide : QueryTriggerInteraction.Ignore;
-            
-            for (var i = 0; i < colliderCount; i++)
-            {
-                if (hitCount >= hits.Length)
-                    break;
-                
-                var col = _colliders3D[i];
-                
-                if (!col)
-                    continue;
-                
-                if (col.isTrigger && queryTriggers == QueryTriggerInteraction.Ignore)
-                    continue;
-                
-                bool isPartOfLayerMask = ((1 << col.gameObject.layer) & layerMask) != 0;
-                
-                if (!isPartOfLayerMask)
-                    continue;
-                
-                if (!_collider3DStates.TryGetValue(col, out var history))
-                    continue;
-
-                bool hasStateA = history.TryGet(tick, out var stateA);
-                bool hasStateB = history.TryGet(tick + 1, out var stateB);
-
-                switch (hasStateA)
-                {
-                    case true when hasStateB:
-                        stateA = stateA.Interpolate(stateB, tickFraction);
-                        break;
-                    case false when hasStateB:
-                        stateA = stateB;
-                        break;
-                    case false: continue;
-                    case true:
-                        break;
-                }
-                
-                if (!stateA.enabled)
-                    continue;
-                
-                var trs = col.transform;
-
-                // Get the transform matrix for the historical position
-                var historicalWorldMatrix = Matrix4x4.TRS(stateA.position, stateA.rotation, stateA.scale);
-                var worldToHistorical = historicalWorldMatrix.inverse;
-
-                // Transform world ray to historical local space
-                var rayHistoricalLocal = new Ray(
-                    worldToHistorical.MultiplyPoint3x4(ray.origin), 
-                    worldToHistorical.MultiplyVector(ray.direction)
-                );
-
-                // Transform historical local ray to current world space for the actual raycast
-                var currentWorldMatrix = trs.localToWorldMatrix;
-                var rayCurrentWorld = new Ray(
-                    currentWorldMatrix.MultiplyPoint3x4(rayHistoricalLocal.origin),
-                    currentWorldMatrix.MultiplyVector(rayHistoricalLocal.direction)
-                );
-
-                if (col.Raycast(rayCurrentWorld, out var hit, maxDistance))
-                {
-                    // Transform hit from current world space to current local space
-                    var currentToLocal = trs.worldToLocalMatrix;
-                    hit.point = currentToLocal.MultiplyPoint3x4(hit.point);
-                    hit.normal = currentToLocal.MultiplyVector(hit.normal);
-    
-                    // Transform hit from current local space to historical world space
-                    hit.point = historicalWorldMatrix.MultiplyPoint3x4(hit.point);
-                    hit.normal = historicalWorldMatrix.MultiplyVector(hit.normal);
-    
-                    hits[hitCount++] = hit;
-                }
-            }
-
-            return hitCount;
-        }
-
-        private int FilterColliders(int hitCount, RaycastHit[] hits)
-        {
-            for (var i = 0; i < hitCount; i++)
-            {
-                var col = hits[i].collider;
-                if (col && _trackedColliders.Contains(col))
-                    hits[i--] = hits[--hitCount];
-            }
-
-            return hitCount;
         }
 
         public void OnPostTick()
