47a3cd606d559141ebe8ea294c07583420dc4f72
BlenMiner <blenminer123@gmail.com>
2025-05-04 02:49:03 +0200

    fix: bit packet, include base classes

--- Assets/Issues/RPCIssue/StaticRpcTest.cs
+++ Assets/Issues/RPCIssue/StaticRpcTest.cs
@@ -1,5 +1,6 @@
 using JetBrains.Annotations;
 using PurrNet;
+using PurrNet.Packing;
 using TriInspector;
 using UnityEngine;
 
@@ -8,12 +9,49 @@ public class StaticRpcTest : NetworkIdentity
     [Button("SendObserverRpc"), UsedImplicitly]
     public void SendRpc()
     {
-        SendObserverRpc("Test with a bunch of data, this is a test, for testing and testing purposes and testing only");
+        var someData = new SomeBaseData
+        {
+            someInt = 1,
+            someString = "Hello"
+        };
+
+        var someData2 = new SomeDerivedData
+        {
+            someInt = 2,
+            someString = "World",
+            someInt2 = 3,
+            someString2 = "!"
+        };
+
+        SendObserverRpc(someData, someData2);
     }
 
     [ObserversRpc(bufferLast: true)]
-    public void SendObserverRpc(string someData)
+    public void SendObserverRpc(SomeBaseData someData, SomeDerivedData someData2)
     {
-        Debug.Log("SendObserverRpc "+ someData);
+        Debug.Log($"someData: {someData}");
+        Debug.Log($"someData2: {someData2}");
+    }
+}
+
+public class SomeBaseData : IPackedAuto
+{
+    public int someInt;
+    public string someString;
+
+    public override string ToString()
+    {
+        return $"{nameof(SomeBaseData)}: {nameof(someInt)}: {someInt}, {nameof(someString)}: {someString}";
+    }
+}
+
+public class SomeDerivedData : SomeBaseData, IPackedAuto
+{
+    public int someInt2;
+    public string someString2;
+
+    public override string ToString()
+    {
+        return $"{nameof(SomeDerivedData)}: {nameof(someInt)}: {someInt}, {nameof(someString)}: {someString}, {nameof(someInt2)}: {someInt2}, {nameof(someString2)}: {someString2}";
     }
 }
