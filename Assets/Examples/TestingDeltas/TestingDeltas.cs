d4e7d725b878afb85bc77b4fb85f5828cc388d46
BlenMiner <blenminer123@gmail.com>
2025-05-14 19:00:39 +0200

    compiler errors

--- Assets/Examples/TestingDeltas/TestingDeltas.cs
+++ Assets/Examples/TestingDeltas/TestingDeltas.cs
@@ -3,23 +3,8 @@ using PurrNet.Modules;
 
 public class TestingDeltas : NetworkIdentity
 {
-    private DeltaMessagerModule GetMessenger(bool asServer)
-    {
-        if (networkManager.TryGetModule<DeltaMessagerFactory>(asServer, out var factory) &&
-            factory.TryGetModule(sceneId, out var messenger))
-        {
-            return messenger;
-        }
-
-        return null;
-    }
-
     protected override void OnSpawned(bool asServer)
     {
         // GetMessenger(asServer).
-    }
-
-    private void OnGUI()
-    {
     }
 }
