d23c0f08e27974a2d558522a99dcd31beb0448bb
BlenMiner <blenminer123@gmail.com>
2025-06-25 16:21:03 +0200

    testing scripts

--- Assets/Examples/TestScriptNB.cs
+++ Assets/Examples/TestScriptNB.cs
@@ -1,5 +1,30 @@
 using PurrNet;
+using PurrNet.Logging;
 
 public class TestScriptNB : NetworkIdentity
 {
+    [PurrButton]
+    public void ServerRpcTesTButton()
+    {
+        ServerRpcTesT();
+    }
+
+    protected override void OnSpawned(bool asServer)
+    {
+        if (!asServer)
+            ServerRpcTesT();
+    }
+
+    [ServerRpc]
+    public void ServerRpcTesT(RPCInfo info = default)
+    {
+        PurrLogger.Log($"ServerRpcTesT called from {info.sender} {isClient} {localPlayer}");
+        TargetRpcT(info.sender);
+    }
+
+    [TargetRpc]
+    public void TargetRpcT(PlayerID target)
+    {
+        PurrLogger.Log($"TargetRpcT called on target {target}");
+    }
 }
