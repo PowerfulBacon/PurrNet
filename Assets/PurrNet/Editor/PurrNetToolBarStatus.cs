db3975d400044966862e12c478966bedd53e8a37
charles <feiyuren233@gmail.com>
2025-07-11 12:58:50 -0400

    Using EditorCoroutines for PurrNetToolBarStatus, and some refactoring

--- Assets/PurrNet/Editor/PurrNetToolBarStatus.cs
+++ Assets/PurrNet/Editor/PurrNetToolBarStatus.cs
@@ -2,12 +2,13 @@ using PurrNet.Transports;
 using UnityEditor;
 using UnityEngine;
 using UnityToolbarExtender;
+using Unity.EditorCoroutines.Editor;
+using System.Collections;
 
 namespace PurrNet.Editor
 {
     public static class PurrNetToolBarStatus
     {
-        private static int _extraDraws;
         private static GUIContent pebblesIcon;
 
         [InitializeOnLoadMethod]
@@ -16,16 +17,28 @@ namespace PurrNet.Editor
             ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
             NetworkManager.onAnyServerConnectionState += OnConnectionStateChanged;
             NetworkManager.onAnyClientConnectionState += OnConnectionStateChanged;
-            var defaultIcon = AssetDatabase.LoadAssetAtPath<Texture2D>(
+            
+            var pebblesTexture = AssetDatabase.LoadAssetAtPath<Texture2D>(
                 "Assets/PurrNet/Editor/Editor Default Resources/Pebbles.png");
-            pebblesIcon = new GUIContent(defaultIcon);
+            pebblesIcon = new GUIContent(pebblesTexture);
         }
 
         private static void OnConnectionStateChanged(ConnectionState state)
         {
-            ToolbarExtender.RequestToolbarRepaint();
-            _extraDraws = 10; // Force a repaint to ensure the toolbar updates
+            EditorCoroutineUtility.StartCoroutineOwnerless(AutoRefreshCoroutine());
         }
+
+        private static IEnumerator AutoRefreshCoroutine()
+        {
+            while (IsTransitioning(NetworkManager.main)) {
+                yield return new EditorWaitForSeconds(0.5f);
+            }
+            ToolbarExtender.RequestToolbarRepaint();
+        }
+
+        private static bool IsTransitioning(NetworkManager? manager) =>
+            manager?.serverState is ConnectionState.Connecting or ConnectionState.Disconnecting ||
+            manager?.clientState is ConnectionState.Connecting or ConnectionState.Disconnecting;
 
         static void OnToolbarGUI()
         {
@@ -33,62 +46,44 @@ namespace PurrNet.Editor
 
             var manager = NetworkManager.main;
 
-            GUILayout.BeginHorizontal("EditorToolbar");
+            GUILayout.BeginHorizontal();
             
             GUILayout.Label(pebblesIcon, GUILayout.Width(22), GUILayout.Height(22));
             GUILayout.Label("PurrNet", GUILayout.ExpandWidth(false));
             
-            ConnectionState? serverState = manager == null ? null : manager.serverState; 
-            bool serverButtonEnabled = manager != null && serverState != ConnectionState.Connecting &&
-                                 serverState != ConnectionState.Disconnecting;
-            string serverButtonText =
-                serverState == ConnectionState.Connecting || serverState == ConnectionState.Disconnecting
-                    ? serverState.ToString()
-                    : (manager?.isServer == true)
-                        ? "Stop Server"
-                        : "Start Server";
-
-            GUI.enabled = serverButtonEnabled;
-            if (GUILayout.Button(serverButtonText, GUILayout.Width(100))) {
-                if (manager.isServer) {
-                    manager.StopServer();
-                }
-                else {
-                    manager.StartServer();
-                }
-            }
-
-            TransportInspector.DrawLed(serverState);
-
-            ConnectionState? clientState = manager == null ? null : manager.clientState;  
-            bool clientButtonEnabled = manager != null && clientState != ConnectionState.Connecting &&
-                                 clientState != ConnectionState.Disconnecting;
-            string clientButtonText =
-                clientState == ConnectionState.Connecting || clientState == ConnectionState.Disconnecting
-                    ? clientState.ToString()
-                    : (manager?.isClient == true)
-                        ? "Stop Client"
-                        : "Start Client";
-
-            GUI.enabled = clientButtonEnabled;
-            if (GUILayout.Button(clientButtonText, GUILayout.Width(100))) {
-                if (manager.isClient) {
-                    manager.StopClient();
-                }
-                else {
-                    manager.StartClient();
-                }
-            }
-
-            TransportInspector.DrawLed(clientState);
+            DrawConnectionButton(manager, true);  // Server
+            DrawConnectionButton(manager, false); // Client
             
             GUILayout.EndHorizontal();
             GUILayout.Space(20);
+        }
 
-            if (_extraDraws > 0) {
-                _extraDraws--;
-                ToolbarExtender.RequestToolbarRepaint();
+        private static void DrawConnectionButton(NetworkManager manager, bool isServer)
+        {
+            var state = isServer ? manager?.serverState : manager?.clientState;
+            var isActive = isServer ? manager?.isServer == true : manager?.isClient == true;
+            var isTransitioning = state is ConnectionState.Connecting or ConnectionState.Disconnecting;
+            
+            string buttonText = isTransitioning ? state.ToString() : 
+                               isActive ? $"Stop {(isServer ? "Server" : "Client")}" : 
+                               $"Start {(isServer ? "Server" : "Client")}";
+
+            GUI.enabled = manager != null && !isTransitioning;
+            if (GUILayout.Button(buttonText, GUILayout.Width(100)))
+            {
+                if (isServer)
+                {
+                    if (manager.isServer) manager.StopServer();
+                    else manager.StartServer();
+                }
+                else
+                {
+                    if (manager.isClient) manager.StopClient();
+                    else manager.StartClient();
+                }
             }
+
+            TransportInspector.DrawLed(state);
         }
     }
 }
