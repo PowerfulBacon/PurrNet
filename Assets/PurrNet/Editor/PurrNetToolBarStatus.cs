343c526cd3eed2f418129ccf6543274d40f0be9d
charles <feiyuren233@gmail.com>
2025-07-11 10:37:55 -0400

    Added server / client toggle buttons to PurrNetToolBarStatus

--- Assets/PurrNet/Editor/PurrNetToolBarStatus.cs
+++ Assets/PurrNet/Editor/PurrNetToolBarStatus.cs
@@ -8,6 +8,7 @@ namespace PurrNet.Editor
     public static class PurrNetToolBarStatus
     {
         private static int _extraDraws;
+        private static GUIContent pebblesIcon;
 
         [InitializeOnLoadMethod]
         static void Init()
@@ -15,6 +16,9 @@ namespace PurrNet.Editor
             ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
             NetworkManager.onAnyServerConnectionState += OnConnectionStateChanged;
             NetworkManager.onAnyClientConnectionState += OnConnectionStateChanged;
+            var defaultIcon = AssetDatabase.LoadAssetAtPath<Texture2D>(
+                "Assets/PurrNet/Editor/Editor Default Resources/Pebbles.png");
+            pebblesIcon = new GUIContent(defaultIcon);
         }
 
         private static void OnConnectionStateChanged(ConnectionState state)
@@ -29,17 +33,59 @@ namespace PurrNet.Editor
 
             var manager = NetworkManager.main;
 
-            GUILayout.BeginHorizontal("helpbox");
+            GUILayout.BeginHorizontal("EditorToolbar");
+            
+            GUILayout.Label(pebblesIcon, GUILayout.Width(22), GUILayout.Height(22));
             GUILayout.Label("PurrNet", GUILayout.ExpandWidth(false));
-            GUILayout.Label("Server", GUILayout.ExpandWidth(false));
-            TransportInspector.DrawLed(manager ? manager.serverState : null);
+            
+            ConnectionState? serverState = manager == null ? null : manager.serverState; 
+            bool serverButtonEnabled = manager != null && serverState != ConnectionState.Connecting &&
+                                 serverState != ConnectionState.Disconnecting;
+            string serverButtonText =
+                serverState == ConnectionState.Connecting || serverState == ConnectionState.Disconnecting
+                    ? serverState.ToString()
+                    : (manager?.isServer == true)
+                        ? "Stop Server"
+                        : "Start Server";
 
-            GUILayout.Label("Client", GUILayout.ExpandWidth(false));
-            TransportInspector.DrawLed(manager ? manager.clientState : null);
+            GUI.enabled = serverButtonEnabled;
+            if (GUILayout.Button(serverButtonText, GUILayout.Width(100))) {
+                if (manager.isServer) {
+                    manager.StopServer();
+                }
+                else {
+                    manager.StartServer();
+                }
+            }
+
+            TransportInspector.DrawLed(serverState);
+
+            ConnectionState? clientState = manager == null ? null : manager.clientState;  
+            bool clientButtonEnabled = manager != null && clientState != ConnectionState.Connecting &&
+                                 clientState != ConnectionState.Disconnecting;
+            string clientButtonText =
+                clientState == ConnectionState.Connecting || clientState == ConnectionState.Disconnecting
+                    ? clientState.ToString()
+                    : (manager?.isClient == true)
+                        ? "Stop Client"
+                        : "Start Client";
+
+            GUI.enabled = clientButtonEnabled;
+            if (GUILayout.Button(clientButtonText, GUILayout.Width(100))) {
+                if (manager.isClient) {
+                    manager.StopClient();
+                }
+                else {
+                    manager.StartClient();
+                }
+            }
+
+            TransportInspector.DrawLed(clientState);
+            
             GUILayout.EndHorizontal();
+            GUILayout.Space(20);
 
-            if (_extraDraws > 0)
-            {
+            if (_extraDraws > 0) {
                 _extraDraws--;
                 ToolbarExtender.RequestToolbarRepaint();
             }
