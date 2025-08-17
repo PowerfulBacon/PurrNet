b17dcd2f75e16d431f9fcbbd9efdddb8e3bdcc41
BlenMiner <blenminer123@gmail.com>
2025-08-18 01:57:53 +0200

    cleanup extension code

--- Assets/PurrNet/Runtime/Utils/ComponentExtensions.cs
+++ Assets/PurrNet/Runtime/Utils/ComponentExtensions.cs
@@ -1,77 +1,72 @@
-using System.Reflection;
 using UnityEngine;
-using UnityEngine.Pool;
+#if !UNITY_2022_3_OR_NEWER
+using System.Reflection;
+using PurrNet.Pooling;
+#endif
 
 public static class ComponentExtensions
 {
     #region Animator
 
+#if !UNITY_2022_3_OR_NEWER
+
     private static readonly FieldInfo _avatarRootField;
-    
+
     static ComponentExtensions()
     {
         _avatarRootField = typeof(Animator).GetField("avatarRoot", BindingFlags.NonPublic | BindingFlags.Instance);
     }
-    
+#endif
+
     public static Transform GetAvatarRoot(this Animator animator)
     {
 #if !UNITY_2022_3_OR_NEWER
         if (animator == null || _avatarRootField == null)
-        {
             return null;
-        }
         return _avatarRootField.GetValue(animator) as Transform;
 #else
-        if (animator == null)
-        {
-            return null;
-        }
-        return animator.avatarRoot;
+        return animator ? animator.avatarRoot : null;
 #endif
     }
 
     #endregion
-    
+
 #if !UNITY_2022_3_OR_NEWER
     public static int GetComponentIndex(this Component component)
     {
-        if (component == null)
-        {
+        if (!component)
             return -1;
-        }
 
-        var components = ListPool<Component>.Get();
-        component.gameObject.GetComponents(component.GetType(), components);
+        using var components = DisposableList<Component>.Create(16);
+        component.gameObject.GetComponents<Component>(components.list);
+
         for (int i = 0; i < components.Count; i++)
         {
             if (components[i] == component)
-            {
-                ListPool<Component>.Release(components);
                 return i;
-            }
         }
 
-        ListPool<Component>.Release(components);
         return -1;
     }
 
     public static T GetComponentAtIndex<T>(this GameObject gameObject, int index) where T : Component
     {
         if (gameObject == null || index < 0)
-        {
             return null;
-        }
 
-        var components = ListPool<T>.Get();
-        gameObject.GetComponents<T>(components);
+        using var components = DisposableList<Component>.Create(16);
+        gameObject.GetComponents<Component>(components.list);
+
         if (index >= components.Count)
-        {
-            ListPool<T>.Release(components);
             return null;
-        }
 
-        ListPool<T>.Release(components);
-        return components[index];
+        if (components[index] == null)
+            return null;
+
+        if (components[index] is T result)
+            return result;
+
+        return null;
     }
 #endif
 }
