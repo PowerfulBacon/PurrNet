ea58d80a26550aa0865228e3c999b6f0ce85eff6
BlenMiner <blenminer123@gmail.com>
2025-10-08 11:34:18 +0200

    refactor menu item tooling to multiple files

---
+++ Assets/PurrNet/Editor/EnableDebugTooling.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+
+namespace PurrNet.Editor
+{
+    public static class EnableDebugTooling
+    {
+#if PURR_DELTA_CHECK
+        [MenuItem("Tools/PurrNet/Debug/Disable Delta Validator", priority = 200)]
+        public static void UninstallDeltaCheck()
+        {
+            SymbolsHelper.RemoveSymbol("PURR_DELTA_CHECK");
+        }
+#else
+        [MenuItem("Tools/PurrNet/Debug/Enable Delta Validator", priority = 200)]
+        public static void InstallDeltaCheck()
+        {
+            SymbolsHelper.AddSymbol("PURR_DELTA_CHECK");
+        }
+#endif
+
+#if PURR_LEAKS_CHECK
+        [MenuItem("Tools/PurrNet/Debug/Disable Leak Detection", priority = 200)]
+        public static void Uninstall()
+        {
+            SymbolsHelper.RemoveSymbol("PURR_LEAKS_CHECK");
+        }
+#else
+        [MenuItem("Tools/PurrNet/Debug/Enable Leak Detection", priority = 200)]
+        public static void Install()
+        {
+            SymbolsHelper.AddSymbol("PURR_LEAKS_CHECK");
+        }
+#endif
+
+#if PURRNET_DEBUG_POOLING
+        [MenuItem("Tools/PurrNet/Debug/Disable Pool Debug", priority = 200)]
+        public static void UninstallPool()
+        {
+            SymbolsHelper.RemoveSymbol("PURRNET_DEBUG_POOLING");
+        }
+#else
+        [MenuItem("Tools/PurrNet/Debug/Enable Pool Debug", priority = 200)]
+        public static void InstallPool()
+        {
+            SymbolsHelper.AddSymbol("PURRNET_DEBUG_POOLING");
+        }
+#endif
+    }
+}
