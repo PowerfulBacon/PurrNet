cacc0c7bc9d501ee7bbbf65d1e1a028145182612
BlenMiner <blenminer123@gmail.com>
2025-10-10 12:38:45 +0200

    fix: only adapt outside of editor

--- Assets/PurrNet/Runtime/Transports/AutomaticCloudSetups.cs
+++ Assets/PurrNet/Runtime/Transports/AutomaticCloudSetups.cs
@@ -8,7 +8,7 @@ namespace PurrNet.Transports
     {
         [SerializeField] private bool _adaptToEdgegap = true;
 
-#if EDGEGAP_PURRNET_SUPPORT && UNITY_SERVER
+#if EDGEGAP_PURRNET_SUPPORT && UNITY_SERVER && !UNITY_EDITOR
         public bool adaptToEdgegap => _adaptToEdgegap;
 #else
         public bool adaptToEdgegap => false;
