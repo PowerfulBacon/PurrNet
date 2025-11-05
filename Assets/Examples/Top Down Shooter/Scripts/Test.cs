6c9275ea409ea4d09d4072ad488fc8628a12862e
BobsiUnity <martin.skovrup@gmail.com>
2025-11-05 20:33:14 +0100

    Modify test

--- Assets/Examples/Top Down Shooter/Scripts/Test.cs
+++ Assets/Examples/Top Down Shooter/Scripts/Test.cs
@@ -1,12 +1,18 @@
-using System;
 using System.Collections.Generic;
 using PurrNet;
 using UnityEngine;
 
 public class Test : NetworkIdentity
 {
-    private Queue<PlayerID> _bots = new();
+    [SerializeField, PurrScene] private string _sceneOne, _sceneTwo; 
     
+    private Queue<PlayerID> _bots = new();
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     protected override void OnSpawned(bool asServer)
     {
         if (!asServer)
@@ -32,39 +38,62 @@ public class Test : NetworkIdentity
 
     private void OnPlayerJoinedScene(PlayerID player, SceneID scene, bool asServer)
     {
+        if (player.id == 1)
+            return;
+
         Debug.Log($"Joined scene: {player} | {scene} | {asServer}");
     }
 
     private void OnPlayerLoadedScene(PlayerID player, SceneID scene, bool asServer)
     {
+        if (player.id == 1)
+            return;
+
         Debug.Log($"Loaded scene: {player} | {scene} | {asServer}");
     }
 
     private void OnPlayerJoined(PlayerID player, bool isReconnect, bool asServer)
     {
+        if (player.id == 1)
+            return;
+
         Debug.Log($"Joined: {player} | {asServer}");
     }
 
     private void OnPlayerUnloadedScene(PlayerID player, SceneID scene, bool asServer)
     {
-        Debug.Log($"Unloaded scene: {player} | {asServer}");
+        if (player.id == 1)
+            return;
+        
+        Debug.Log($"Unloaded scene: {player} | {scene} | {asServer}");
     }
 
     private void OnPlayerLeftScene(PlayerID player, SceneID scene, bool asServer)
     {
-        Debug.Log($"Left scene: {player} | {asServer}");
+        if (player.id == 1)
+            return;
+
+        Debug.Log($"Left scene: {player} | {scene} | {asServer}");
     }
 
     private void OnPlayerLeft(PlayerID player, bool asServer)
     {
+        if (player.id == 1)
+            return;
+
         Debug.Log($"Left: {player} | {asServer}");
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X)) 
+        if (Input.GetKeyDown(KeyCode.X))
             _bots.Enqueue(networkManager.playerModule.CreateBot());
         if (Input.GetKeyDown(KeyCode.C)) 
             networkManager.playerModule.KickPlayer(_bots.Dequeue());
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            networkManager.sceneModule.LoadSceneAsync(_sceneOne);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            networkManager.sceneModule.LoadSceneAsync(_sceneTwo);
     }
 }
