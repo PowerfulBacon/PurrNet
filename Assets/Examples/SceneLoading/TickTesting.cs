b788fd6ff9ee419a88c663335cbe8e4fdc22bdb0
BlenMiner <blenminer123@gmail.com>
2025-11-12 17:14:44 +0100

    more testing code

--- Assets/Examples/SceneLoading/TickTesting.cs
+++ Assets/Examples/SceneLoading/TickTesting.cs
@@ -5,6 +5,24 @@ public class TickTesting : NetworkIdentity, ITick
 {
     [SerializeField] private ulong _tick;
 
+    [SerializeField] private SyncVar<bool> _testingSomeValue;
+
+    protected override void OnSpawned()
+    {
+        _testingSomeValue.onChanged += OnChanged;
+    }
+
+    private void OnChanged(bool value)
+    {
+        Debug.Log($"Changed to {value}");
+    }
+
+    [PurrButton]
+    public void ToggleTestingValue()
+    {
+        _testingSomeValue.value = !_testingSomeValue.value;
+    }
+
     public void OnTick(float delta)
     {
         ++_tick;
